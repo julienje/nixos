c62d3d6c96fd71ecadf3f74cc3543cba97ceaef5
Julien Jeanneret <julien.jeanneret@outlook.com>
2025-06-07 11:19:54 +0200

    feat: update flake.lock and flake.nix, remove unused vm-sbb configurations

--- flake.nix
+++ flake.nix
@@ -4,38 +4,12 @@
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-pinneda.url = "github:NixOS/nixpkgs/336eda0d07dc5e2be1f923990ad9fdb6bc8e28e3";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixpkgs-pinneda,  nix-vscode-extensions, ... }@inputs: {
-
-    nixosConfigurations.vm-sbb = nixpkgs.lib.nixosSystem rec {
-      system = "aarch64-linux";
-      specialArgs = { 
-        pkgs-pinneda = import nixpkgs-pinneda {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      };
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./hosts/vm-sbb
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.julien = ./home/home-sbb.nix;
-
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-        }
-      ];
-    };
+  outputs = { self, nixpkgs, home-manager,  nix-vscode-extensions, ... }@inputs: {
 
     nixosConfigurations.vm-perso = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
